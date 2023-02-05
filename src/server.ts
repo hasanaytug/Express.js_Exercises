166b3a909fc24c843cd9c2aaa569caffb5e8b5ee
Hasan <hasan.aytug@gmail.com>
2023-02-05 20:48:35 +0300

    exercise-12 done

--- src/server.ts
+++ src/server.ts
@@ -27,9 +27,40 @@ let planets: Planets = [
   },
 ];
 
-app.get("/", (req, res) => {
-  console.log(req);
+app.get("/api/planets", (req, res) => {
   res.send(planets);
+});
+
+app.get("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+
+  const planet = planets.find((p) => p.id === Number(id));
+  res.send(planet);
+});
+
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+  const newPlanet = { id, name };
+
+  planets = [...planets, newPlanet];
+  res.status(200).json({ msg: "New planet created successfully" });
+});
+
+app.put("/api/planets/:id", (req, res) => {
+  const { name } = req.body;
+  const { id } = req.params;
+
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+
+  res.status(200).json({ msg: "New planet was updated successfully" });
+});
+
+app.delete("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+
+  planets = planets.filter((p) => p.id !== Number(id));
+
+  res.status(200).json({ msg: "New planet was deleted successfully" });
 });
 
 app.listen(port, () => {
